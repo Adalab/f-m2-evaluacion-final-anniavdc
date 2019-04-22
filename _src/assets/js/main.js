4720db7c4fab9264a69817929b2d54d8372bbc39
anniavdc <anva1189@gmail.com>
2019-04-22 20:16:07 +0200

    Add favorites function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -10,8 +10,12 @@
 const inputElement = document.getElementById('search');
     //boton
 const buttonElement = document.querySelector('.button');
-    //ul
-const listElement = document.querySelector('.list');
+    //ul shows
+const listElement = document.querySelector('.list-shows');
+    //ul favorites
+const listFavElement = document.querySelector('.list-favorites');
+    //array vacío
+const favoritesArr = [];
 
 //fetch a URL de TVMaze
 function handlerClick(){
@@ -29,7 +33,7 @@ fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`)
         const showImageObject = showObject.image;
         
         const showItem = document.createElement('li');
-        showItem.setAttribute('class', 'list__item');
+        showItem.setAttribute('class', 'show__item');
         const showTitle = document.createTextNode(showObject.name);
         const showImage = document.createElement('img');
         showItem.appendChild(showImage);
@@ -45,30 +49,49 @@ fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`)
             showImage.src = showOwnImage; 
         }  
     }
-    const items = document.querySelectorAll('list__item');
+    const items = document.querySelectorAll('.show__item');
     handlerItem(items);
+    console.log(favoritesArr);
+    
 })
 
 }
 //Agregar un listener al botón
 buttonElement.addEventListener('click', handlerClick);
-     
 
 //Favoritos: al hacer clic sobre un resultado el color de fondo y el de fuente se intercambian.
-    //Crear un listado (array) con las series favoritas que almacenamos en una variable. Se mostrará en la parte izquierda de la pantalla, debajo del formulario de búqueda.
-    //si volvemos a realizar una nueva búsqueda, los favoritos se irán acumulando en nuestra lista.
-
-function changeClass(event){
+function changeClass(event) {
     const element = event.currentTarget;
-    element.classList.toggle('favorites');
+    element.classList.toggle('favorites'); 
+
+    favorites(element);
 }
 
-function handlerItem(items){  
-    for(let i = 0; i < items.length; i++){
-        items[i].addEventListener('click', changeClass);
+function handlerItem(items){
+    for (let i = 0; i < items.length; i++) {
+      items[i].addEventListener('click', changeClass);
     }
+    console.log(favoritesArr);
+}
+  
+
+//Crear un listado (array) con las series favoritas que almacenamos en una variable. Se mostrará en la parte izquierda de la pantalla, debajo del formulario de búqueda.
+
+function favorites(element){
+        if(element.classList.contains('favorites')){
+            favoritesArr.push(element);
+
+            const favoritesList = document.createElement('li');
+            const favoritesContent = document.createTextNode(element.innerText);
+            const favoritesImage = document.createElement('img');
+            favoritesList.appendChild(favoritesContent);
+            favoritesList.appendChild(favoritesImage);
+            listFavElement.appendChild(favoritesList);   
+        }
+        
 }
 
+//si volvemos a realizar una nueva búsqueda, los favoritos se irán acumulando en nuestra lista.
 
 //Vamos a almacenar el listado de favoritos en el localStorage. De esta forma, al recargar la página el listado de favoritos se mantiene
 
