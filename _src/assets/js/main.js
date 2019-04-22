035ca489f45e9c5d9e4928f4fd0cd3cd7d06b66f
anniavdc <anva1189@gmail.com>
2019-04-22 15:37:31 +0200

    Write basic guide

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('>> Ready :)');
+//Hacer HTML básico
+    //h1 Buscador de series
+    //input
+    //boton
+    //ul
+//Recoger los elementos en js
+    //input
+    //boton
+    //ul
+
+//Agregar un listener al botón
+
+//fetch a URL de TVMaze
+    //Ver que tipo de data nos regresa el fetch   
+    //Por cada show contenido en el resultado de búsqueda debemos pintar una tarjeta donde mostramos una imagen de la serie y el título.
+    //if
+        //el show no tiene imagen, crear una imagen de relleno con placeholder https://via.placeholder.com/210x295/ffffff/666666/?text=TV
+
+//Favoritos: al hacer clic sobre un resultado el color de fondo y el de fuente se intercambian.
+    //Crear un listado (array) con las series favoritas que almacenamos en una variable. Se mostrará en la parte izquierda de la pantalla, debajo del formulario de búqueda.
+    //si volvemos a realizar una nueva búsqueda, los favoritos se irán acumulando en nuestra lista.
+
+//Vamos a almacenar el listado de favoritos en el localStorage. De esta forma, al recargar la página el listado de favoritos se mantiene
+
+//Hacer CSS
